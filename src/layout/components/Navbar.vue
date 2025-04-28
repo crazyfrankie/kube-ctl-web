2327c2dd166f5de1eaabe210bc6db3e8c03f10fb
crazyfrankie <axu9417@gmail.com>
2025-04-28 21:42:29 +0800

    fix(avatar)

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,29 +1,23 @@
 <template>
   <div class="navbar">
     <hamburger :is-active="sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar" />
-
     <breadcrumb class="breadcrumb-container" />
 
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img :src="avatarUrl" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
-            <el-dropdown-item>
-              Home
-            </el-dropdown-item>
+            <el-dropdown-item>首页</el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
+          <a target="_blank" href="https://github.com/crazyfrankie/kube-ctl">
             <el-dropdown-item>Github</el-dropdown-item>
           </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
-          </a>
           <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
+            <span style="display:block;">退出登录</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
@@ -45,7 +39,13 @@ export default {
     ...mapGetters([
       'sidebar',
       'avatar'
-    ])
+    ]),
+    avatarUrl() {
+      if (this.avatar && (this.avatar.startsWith('./') || this.avatar.startsWith('/'))) {
+        return require('@/assets/avatars/1.jpg')
+      }
+      return this.avatar || require('@/assets/avatars/1.jpg')
+    }
   },
   methods: {
     toggleSideBar() {
