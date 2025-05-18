7003f07c3f084048fefb73cd02fa41b775edaaf8
crazyfrank <axu9417@gmail.com>
2025-05-18 17:34:09 +0800

    feat(public):update title and icon

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = defaultSettings.title || 'vue Admin Template'
+const name = defaultSettings.title
 
 const port = process.env.port || process.env.npm_config_port || 9528
 
